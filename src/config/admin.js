fdf2de3cbbf8cc39ac586aa34fd855a792a8d4fa
Alanashu07 <alanashu07@gmail.com>
2026-09-20 15:26:42 +0530

    refactor(admin): Update import paths for check-in and command queue services

--- src/config/admin.js
+++ src/config/admin.js
@@ -16,8 +16,8 @@ import session from 'express-session';
 import connectPgSimple from 'connect-pg-simple';
 import prisma from './db.js';
 import env from './env.js';
-import checkinService from '../services/checkin.service.js';
-import commandQueue from '../services/deviceCommandQueue.service.js';
+import checkinService from '../modules/device/checkin.service.js';
+import commandQueue from '../modules/device/device-command-queue.service.js';
 
 AdminJS.registerAdapter({ Database, Resource });
 
